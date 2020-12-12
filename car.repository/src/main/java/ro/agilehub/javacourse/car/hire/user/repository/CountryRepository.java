4b70eb7832ffcaa74c1a0180abf72052d4685636
ciprian.buzduga <ciprian.buzduga@morecommerce.com>
2020-12-12 11:22:27 +0200

    Remove unused import.

--- car.repository/src/main/java/ro/agilehub/javacourse/car/hire/user/repository/CountryRepository.java
+++ car.repository/src/main/java/ro/agilehub/javacourse/car/hire/user/repository/CountryRepository.java
@@ -2,7 +2,6 @@ package ro.agilehub.javacourse.car.hire.user.repository;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-import ro.agilehub.javacourse.car.hire.fleet.document.CarDoc;
 import ro.agilehub.javacourse.car.hire.patch.repository.DocumentPatchRepository;
 import ro.agilehub.javacourse.car.hire.user.document.CountryDoc;
 
